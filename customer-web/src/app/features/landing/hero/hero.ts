af7cc7895a48f2b871f6cae92ea7e9384431c316
Prakhar Sahu <sahuprakhar022003@gmail.com>
2026-08-15 16:42:12 +0530

    refactor(hero-section): Restructure code

--- customer-web/src/app/features/landing/hero/hero.ts
+++ customer-web/src/app/features/landing/hero/hero.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {
-  faMagnifyingGlass, faLocationDot
+  faLocationDot,
+  faMagnifyingGlass
 } from '@fortawesome/free-solid-svg-icons';
+import { Button } from '../../../shared/components/button/button';
+
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [FontAwesomeModule, RouterLink],
+  imports: [FontAwesomeModule,Button],
   templateUrl: './hero.html',
   styleUrl: './hero.sass',
 })
 export class Hero {
+
   faLocationDot = faLocationDot;
   faMagnifyingGlass = faMagnifyingGlass;
+
+  private readonly router = inject(Router);
+
+  findFood(): void {
+    this.router.navigate(['/india']);
+  }
 }
