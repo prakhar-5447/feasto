953548ba87784952130cee172b604d720135cb17
Prakhar Sahu <sahuprakhar022003@gmail.com>
2026-08-16 14:19:25 +0530

    refactor(footer): Restructure code

--- customer-web/src/app/shared/components/footer/footer.ts
+++ customer-web/src/app/shared/components/footer/footer.ts
@@ -1,17 +1,31 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faFacebook, faTwitter, faYoutube, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faYoutube
+} from '@fortawesome/free-brands-svg-icons';
+
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [
+    FontAwesomeModule,
+    RouterLink
+  ],
   templateUrl: './footer.html',
   styleUrl: './footer.sass',
 })
 export class Footer {
-  faFacebook = faFacebook;
-  faTwitter = faTwitter;
-  faInstagram = faInstagram;
-  faYoutube = faYoutube;
+
+  readonly faFacebook = faFacebook;
+  readonly faInstagram = faInstagram;
+  readonly faTwitter = faTwitter;
+  readonly faYoutube = faYoutube;
+
+  readonly currentYear = new Date().getFullYear();
 }
